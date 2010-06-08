710066cdbe1d369c78bfa2373194077c256f4ac4
Lars Olesen <lars@intraface.dk>
2010-06-08 15:43:10 +0000

    Tagged version 1.0.2.

--- src/www/config.local.example.php
+++ src/www/config.local.example.php
@@ -1,5 +1,5 @@
 <?php
-$GLOBALS['include_path'] = dirname(__FILE__) . '/../' . PATH_SEPARATOR . get_include_path();
+$GLOBALS['include_path'] = dirname(__FILE__) . PATH_SEPARATOR . dirname(__FILE__) . '/../' . PATH_SEPARATOR . get_include_path();
 ini_set('include_path', $GLOBALS['include_path']);
 $GLOBALS['intraface_private_key'] = "L9FtAdfAu8QwLSChGZehzeZwiAhXNwsqwWIMZF4avCw6jY6HN2G";
 $GLOBALS['intraface_site_id'] = 35;
