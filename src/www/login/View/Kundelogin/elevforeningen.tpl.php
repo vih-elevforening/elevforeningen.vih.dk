024db056e1e77e01475631e6da4193d0bc9a64d3
Lars Olesen <lars@intraface.dk>
2010-07-24 10:34:31 +0200

    Outcommented unused text.

--- src/www/login/View/Kundelogin/elevforeningen.tpl.php
+++ src/www/login/View/Kundelogin/elevforeningen.tpl.php
@@ -5,20 +5,23 @@
 	<p class="explanation">På denne side kan du ændre dine adresseoplysninger og tilmelde dig Elevstævnet.</p>
 
 	<?php echo $medlemsoplysninger; ?>
-	
+
 </div>
 
 <div id="content-sub">
 
+    <?php /*
 	<p>
 		Der bør være et link, hvor man kan slette sig selv fra Elevforeningens register - no questions asked.
 	</p>
-	
+
 	<p>
 		Hvis man har blog - link til blog hos FFD, ellers
 		Reklame for blog hos FFD.
 		http://www.hojskolerne.dk/opret-profil.aspx
 	</p>
+	*/
+	?>
 
 	<p id="call">
 		Spørgsmål? &mdash; skriv til
